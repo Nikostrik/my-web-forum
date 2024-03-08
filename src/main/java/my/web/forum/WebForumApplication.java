d817a627e2c31b89b8d68977a8ab476134c7dd5c
Nikita Ostrenskyi <nikostrik2003@gmail.com>
2024-03-08 21:11:26 +0200

    added configuration

---
+++ src/main/java/my/web/forum/WebForumApplication.java
@@ -0,0 +1,12 @@
+package my.web.forum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebForumApplication.class, args);
+	}
+}
---
+++ src/test/java/my/web/forum/WebForumApplicationTests.java
@@ -0,0 +1,13 @@
+package my.web.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
